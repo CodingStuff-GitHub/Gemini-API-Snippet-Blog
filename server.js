345267881ee2eb64fd60a640adc8076e2fec2045
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2024-05-10 13:05:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error : ${err}`);
+  console.log("Shutting Down the server due to uncaughtException");
+  process.exit(1);
+});
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: "config.env" });
+}
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post("/summarize", async (req, res) => {
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = req.body.text;
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    const text = response.text();
+    res.json({ text });
+  } catch (error) {
+    console.error(error.response.data);
+    res.status(500).json({ error: "Summary Generation Failed." });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
